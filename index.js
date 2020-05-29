4a4f117ee7092c160870c534fb3866285ff28ca4
Marie Hrabovska <marie.hrabovska@gmail.com>
2020-05-29 14:43:56 +0200

    3. cast ukolu. Otestovano. Funguje.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 let osoba1 = {
   jmeno: 'Alena',
-  uspory: 53000
+  uspory: 5300
 };
 let osoba2 = {
   jmeno: 'Karolína',
@@ -12,35 +12,18 @@ let osoba2 = {
 const vyletHavaj = (osoba1, osoba2) => {
   if (osoba1.uspory + osoba2.uspory >= 100000) {
     if (osoba1.uspory >= 50000 && osoba2.uspory >= 50000) {
-      return `Kamarádky mají dohromady alespoň 100 000 Kč, každá má alespoň 50 000 Kč, a můžou jet na Havaj.`;
+      return `Kamarádky mají dohromady alespoň 100000 Kč, každá má alespoň 50 000 Kč, a můžou jet na Havaj.`;
     } else if (osoba1.uspory < 50000) {
-      return `Kamarádky majídohromady alespoň 100 000 Kč, ale ${osoba1.jmeno} má pouze ${osoba1.uspory} Kč, tudíž bude dlužit kamarádce ${(50000 - osoba1.uspory)} Kč, na Havaj holky odjet můžou.`;
+      return `Kamarádky majídohromady alespoň 100000 Kč, ale ${osoba1.jmeno} má pouze ${osoba1.uspory} Kč, tudíž bude dlužit kamarádce ${(50000 - osoba1.uspory)} Kč, na Havaj holky odjet můžou.`;
     } else {
-      return `Kamarádky mají dohromady alespoň 100 000 Kč, ale ${osoba2.jmeno} má pouze ${osoba2.uspory} Kč, tudíž bude dlužit kamarádce ${(50000 - osoba2.uspory)} Kč, na Havaj holky odjet můžou.`
+      return `Kamarádky mají dohromady alespoň 100000 Kč, ale ${osoba2.jmeno} má pouze ${osoba2.uspory} Kč, tudíž bude dlužit kamarádce ${(50000 - osoba2.uspory)} Kč, na Havaj holky odjet můžou.`;
     }
 
+  } else if (osoba1.uspory < 50000 && osoba2.uspory < 50000) {
+    return `Kamarádky musí dohromady našetřit ${(100000 - osoba1.uspory - osoba2.uspory)} Kč, aby měly 100000 Kč, a mohly odjet na Havaj. ${osoba1.jmeno} musí našetřit alespoň ${(50000 - osoba1.uspory)} Kč a ${osoba2.jmeno} musí našetřit alespoň ${(50000 - osoba2.uspory)} Kč.`;
+  } else if (osoba1.uspory < 50000) {
+    return `${osoba1.jmeno} má našetřeno ${osoba1.uspory} Kč a ${osoba2.jmeno} ${osoba2.uspory} Kč, tudíž na Havaj ještě jet nemůžou. ${osoba1.jmeno} musí našetřit alespoň ${(100000 - osoba1.uspory - osoba2.uspory)} Kč, aby mohly jet, a bude dlužit kamarádce ještě ${(osoba2.uspory - 50000)} Kč.`;
   } else {
-    return `Kamarádky nemají dohromady 100 000 Kč, a na Havaj jet ještě nemůžou.`;
+    return `${osoba1.jmeno} má našetřeno ${osoba1.uspory} Kč a ${osoba2.jmeno} ${osoba2.uspory} Kč, tudíž na Havaj ještě jet nemůžou. ${osoba2.jmeno} musí našetřit alespoň ${(100000 - osoba2.uspory - osoba1.uspory)} Kč, aby mohly jet, a bude dlužit kamarádce ještě ${(osoba1.uspory - 50000)} Kč.`;
   }
 }
-
-
-
-
-/*
-if (osoba1.uspory >= 50000 && osoba2.uspory >= 50000) {
-  return `Holky mají každá naspořeno alespoň 50 000 Kč a můžou jet na Havaj.`;
-} else {
-  if (osoba1.uspory < 50000) {
-  return `Holky můžou jet na Havaj, ale ${osoba1.jmeno} bude dlužit kamarádce ${50000 - osoba1.uspory} Kč.`;
-} else {  ((osoba1.uspory <= 50000) && (osoba2.uspory >= 50000)) {
-  return `Holky můžou jet na Havaj, ale ${osoba1.jmeno} bude dlužit ${50000 - osoba1.uspory} Kč ${osoba2.jmeno}.`;
-}
-} else if ((osoba1.uspory <= 50000) && (osoba2.uspory <= 50000)) {
-return `Žádná Havaj. Holky musí dohromady našetřit ještě ${100000 - osoba1.uspory - osoba2.uspory}, aby mohly na Havaj odjet. ${osoba1.jmeno} musí našetřit ${50000 - osoba1.uspory}, ${osoba2.jmeno} musí našetřit ${50000 - osoba2.uspory}.`;
-} else if (osoba1.uspory >= 50000) {
-return `Žádná Havaj. Holky musí dohromady našetřit ještě ${100000 - osoba1.uspory - osoba2.uspory}, aby mohly na Havaj odjet. ${osoba2.jmeno} musí našetřit ${50000 - osoba2.uspory}, ${osoba1.jmeno} už má našetřeno a může kamarádce půjčit ${osoba1.uspory - 50000}.`;
-} return `Žádná Havaj. Holky musí dohromady našetřit ještě  ${100000 - osoba1.uspory - osoba2.uspory}, aby mohly na Havaj odjet. ${osoba1.jmeno} musí našetřit ${50000 - osoba1.uspory}, ${osoba2.jmeno} už má našetřeno a může kamarádce půjčit ${osoba2.uspory - 50000}.`;
-};
-
-*/
